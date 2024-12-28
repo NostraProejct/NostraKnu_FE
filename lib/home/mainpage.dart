f224097a7d6e3b1991196d2b46e8fe6327a2ed42
kimjunhee <manuna530@gmail.com>
2024-12-28 20:08:53 +0900

    FEAT / CommunityMain 밑 그림

---
+++ lib/home/mainpage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:nostra/community/communityMain.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int curPageIndex = 0;
+
+  final List<Widget> pages = [ Text("Here is home page"), CommunityMain() ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: pages[curPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: curPageIndex,
+          onTap: (idx) {
+            setState(() {
+              curPageIndex = idx;
+            });
+          },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            label: 'Community',
+          ),
+        ],
+      ),
+    );
+  }
+}
