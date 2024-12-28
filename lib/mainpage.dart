83179962c59050b00c40df9fd9b579b160ff6443
kimjunhee <manuna530@gmail.com>
2024-12-28 10:33:05 +0900

    FEAT / 기능 초기화

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nostra/pages/HomeDirectory/Home.dart';
-import 'package:nostra/pages/Map.dart';
-import 'package:nostra/pages/MyTrip.dart';
-import 'package:nostra/pages/Community.dart';
-import 'package:nostra/pages/Menu.dart';
-
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -13,56 +7,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int _selectedIndex = 0;
   List<BottomNavigationBarItem> bottomItems=[
     BottomNavigationBarItem(
         label: 'Home',
         icon: Icon(Icons.home_outlined)
     ),
-    BottomNavigationBarItem(
-        label: 'Map',
-        icon: Icon(Icons.map_outlined)
-    ),
-    BottomNavigationBarItem(
-        label: 'MyTrip',
-        icon: Icon(Icons.add_box_outlined)
-    ),
-    BottomNavigationBarItem(
-        label: 'Community',
-        icon: Icon(Icons.chat_bubble_outlined)
-    ),
-    BottomNavigationBarItem(
-        label: 'Menu',
-        icon: Icon(Icons.menu_outlined)
-    )
-  ];
-  List pages = [
-    Home(),
-    Map(),
-    MyTrip(),
-    Community(),
-    Menu(),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white, // bottomNavigationBar 배경색
-        selectedItemColor : Colors.black,
-        unselectedItemColor: Colors.grey.withOpacity(.60),
-        selectedFontSize: 14,
-        unselectedFontSize: 10,
-        currentIndex: _selectedIndex,
-        onTap: (int index){
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: bottomItems,
-      ),
     );
   }
 }
-
