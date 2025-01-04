7004ce0dfcb82e4c4f4c269190b5f2452334cd38
kimjunhee <manuna530@gmail.com>
2025-01-04 09:03:08 +0900

    FIX / 게시글 작성 디자인 마무리

--- lib/community/write/communityWrite.dart
+++ lib/community/write/communityWrite.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CommunityWrite extends StatefulWidget {
   const CommunityWrite({super.key});
@@ -10,6 +13,23 @@ class CommunityWrite extends StatefulWidget {
 class _CommunityWrite extends State<CommunityWrite> {
   List<String> dropDownList = ['정보 게시판', '취업ㆍ진로 게시판', '대외활동 게시판'];
   String dropdownValue = '정보 게시판';
+  final ImagePicker _picker = ImagePicker();
+  List<File> _imageFiles = [];
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final XFile? image = await _picker.pickImage(source: source);
+      if (image != null) {
+        setState(() {
+          _imageFiles.add(File(image.path));
+        });
+      } else {
+        print('이미지 선택이 취소되었습니다.');
+      }
+    } catch (e) {
+      print('이미지 선택 중 오류 발생: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +64,7 @@ class _CommunityWrite extends State<CommunityWrite> {
                       }).toList(),
                       onChanged: (String? value) {
                         setState(() {
-                          dropdownValue = value!; // 선택된 값을 업데이트
+                          dropdownValue = value!;
                         });
                       },
                     ),
@@ -124,21 +144,21 @@ class _CommunityWrite extends State<CommunityWrite> {
                         height: 280,
                         decoration: BoxDecoration(
                           color: Color(0xFFFAF5F2),
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(5), // border radius를 5로 줄임
                         ),
                         child: TextField(
                           maxLines: null,
-                          minLines: 10,
+                          minLines: 20,
+                          style: TextStyle(fontSize: 11),
                           decoration: InputDecoration(
-                            hintText: '게시글을 입력하세요...',
+                            hintText: '게시글을 입력하세요',
                             hintStyle: TextStyle(color: Colors.grey),
-                            border: InputBorder.none,
+                            border: InputBorder.none, // 테두리 없애기
                             filled: true,
                             fillColor: Color(0xFFFAF5F2),
                             contentPadding: EdgeInsets.all(16),
                           ),
-                        )
-
+                        ),
                       ),
                     )
 
@@ -153,6 +173,84 @@ class _CommunityWrite extends State<CommunityWrite> {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/camera.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  // 사진 추가할 수 있는 Container
+                  GestureDetector(
+                    onTap: () {
+                      // 이미지 선택 기능을 호출
+                      _pickImage(ImageSource.gallery); // 갤러리에서 이미지 선택
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(top: 17),
+                      width: 370,
+                      height: 172,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        children: [
+                          // 기본 카메라 아이콘을 표시하는 부분
+                          _imageFiles.isEmpty
+                              ? Container(
+                            width: 100,
+                            height: 100,
+                            margin: EdgeInsets.only(left: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 2,
+                              ),
+                            ),
+                          )
+                              : Container(),
+                          // 선택된 이미지가 있으면 표시
+                          ..._imageFiles.map((file) {
+                            return Container(
+                              margin: EdgeInsets.only(left: 20),
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: FileImage(file),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
