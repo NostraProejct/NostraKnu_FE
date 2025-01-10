a51144a2f8674415bdba19fc89caa0d3b5d6ba74
dj-jun0918 <rare7287@gmail.com>
2025-01-10 11:22:17 +0900

    설정 창 프로필 박스 화면 구현

--- lib/setting/settingPage.dart
+++ lib/setting/settingPage.dart
@@ -94,13 +94,24 @@ class _SettingPage extends State<SettingPage> {
         Container(
           width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              Text("계정"),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("계정"),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.arrow_forward_ios)
+                  const Text(
+                    "계정 관리",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                    },
+                  ),
                 ],
               ),
             ],
