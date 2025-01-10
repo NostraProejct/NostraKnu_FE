433a7748dc43567336cb8bb5b35642bf8890e4e6
HyeonGwon <hyun80945@gmail.com>
2025-01-10 20:59:32 +0900

    홈/취업,대외활동 FE_5 2025/1/10

---
+++ lib/setting/manageProfile/settingManage.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nostra/setting/manageProfile/stepper.dart';
+
+class SettingManage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SettingManage();
+}
+
+class _SettingManage extends State<SettingManage> {
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        appBar: AppBar(
+          title:  Stack(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 50),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '커뮤니티',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              context.pop();
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  width: screenWidth,
+                  height: 220,
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF8F8F8),
+                  ),
+                  child: Stack(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Align(
+                            alignment: Alignment.center,
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage('assets/images/Profile_image.jpg'),
+                              radius: 70.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: Icon(Icons.settings),
+                          onPressed: () {
+                          },
+                        ),
+                      )
+                    ],
+                  )
+              ),
+              Container(
+                width: screenWidth,
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '내정보',
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: screenWidth,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('닉네임', style: TextStyle(fontSize: 11),),
+                          makeOptionsBox('Lamu_01'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: screenWidth,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('이메일', style: TextStyle(fontSize: 11),),
+                          makeOptionsBox('manuna530@gmail.com'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: screenWidth,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('휴대폰 번호', style: TextStyle(fontSize: 11,),),
+                          makeOptionsBox('010-2998-7438'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: screenWidth,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('소개', style: TextStyle(fontSize: 11),),
+                          makeOptionsBox('나를 소개해주세요'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: screenWidth,
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '공개 범위',
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: CustomStepper(),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+
+  Widget makeOptionsBox(String title)  {
+    return InkWell(
+      onTap: () {
+      },
+      splashColor: Colors.blue.withOpacity(0.3),
+      highlightColor: Colors.blue.withOpacity(0.1),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(25, 30),
+                    side: BorderSide(
+                      color: Colors.black,
+                      width: 1.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {
+                    // ???
+                  },
+                  child: Text(
+                    '변경',
+                    style: TextStyle(color: Colors.black, fontSize: 10),
+                  ),
+                )
+
+              ],
+            ),
+            SizedBox(
+              height: 1,
+              child: Container(
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/setting/settingPage.dart
+++ lib/setting/settingPage.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
 
   @override
   State<StatefulWidget> createState() => _SettingPage();
@@ -10,9 +12,267 @@ class SettingPage extends StatefulWidget {
 class _SettingPage extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('asdjf'),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Color(0xFFF8F8F8),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/Profile_image.jpg'),
+                          radius: 70.0,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "유저 아이디",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "팔로워",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "팔로잉",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10.0),
+                // 프로필 수정 버튼
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    OutlinedButton(
+                      onPressed: () {
+                        context.go('/settings/manage');
+                      },
+                      child: const Text(
+                        "프로필 관리",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 2.0),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "프로필 공유",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('계정', style: TextStyle(color: Colors.grey),),
+                    makeOptionsBox("계정 관리"),
+                  ],
+                ),
+
+                SizedBox(height: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('일반', style: TextStyle(color: Colors.grey),),
+                    makeOptionsBoxWithPic('asset/images/Lan.png', "글자/언어"),
+                    SizedBox(height: 10,),
+                    makeOptionsBoxWithPic('asset/images/Notification.png', "알림/권한"),
+                    SizedBox(height: 10,),
+                    makeOptionsBoxWithPic('asset/images/Option.png', "기타"),
+                  ],
+                ),
+
+                SizedBox(height: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('정보', style: TextStyle(color: Colors.grey),),
+                    makeOptionsBoxWithPic('asset/images/Lan.png', "공지사항"),
+                    SizedBox(height: 10,),
+                    makeOptionsBoxWithPic('asset/images/Notification.png', "앱 관리"),
+                    SizedBox(height: 10,),
+                    makeOptionsBoxWithPic('asset/images/Option.png', "개인정보처리방침"),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      )
+                  ),
+                  onPressed: () {
+
+                  },
+                  child: Center(
+                    child: Text('로그아웃', style: TextStyle(color: Colors.black),),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text('계정 탈퇴',
+                  style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.grey
+                  ),),
+              ),
+              SizedBox(height: 10,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget makeOptionsBox(String title)  {
+    return InkWell(
+      onTap: () {
+      },
+      splashColor: Colors.blue.withOpacity(0.3),
+      highlightColor: Colors.blue.withOpacity(0.1),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_forward_ios),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 1,
+              child: Container(
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeOptionsBoxWithPic(String imgPath, String title)  {
+    return InkWell(
+      onTap: () {
+
+      },
+      splashColor: Colors.blue.withOpacity(0.3),
+      highlightColor: Colors.blue.withOpacity(0.1),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/Notification.png', width: 30,),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_forward_ios),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 1,
+              child: Container(
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
